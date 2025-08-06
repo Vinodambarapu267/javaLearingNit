7c59adeae6defab08043873252200cfb7bdea14e
Vinodambarapu267 <ambarapuvinod@gmail.com>
2025-08-06 22:59:43 +0530

    implemented switch Method to view Specific people details

--- src/main/java/payrollsystem/TaxCalculation.java
+++ src/main/java/payrollsystem/TaxCalculation.java
@@ -1,5 +1,7 @@
 package payrollsystem;
 
+import java.util.Scanner;
+
 public class TaxCalculation {
     public static void main(String[] args) {
         Employee employee = new Employee(121,"vinu",40000,2000,5000);
@@ -10,18 +12,35 @@ public class TaxCalculation {
 //        System.out.println("Trainer Tax: "+trainer.calculateGrossSalary());
         Sourcing sourcing = new Sourcing(99,"dev",50000,5000,4000,10,9,2000);
 //        System.out.println("Sourcing Tax: "+sourcing.calculateGrossSalary());
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----------------------");
+        System.out.println("1. Employee");
+        System.out.println("2. Manager");
+        System.out.println("3. Trainer");
+        System.out.println("4. Sourcing");
+        System.out.println("select from above options: ");
+        int value =sc.nextInt();
+        switch (value){
+            case 1->{System.out.println("Employee Details:"+employee);
+                System.out.println("employee :"+TaxUtil.calculateTax(employee));//tax
+                System.out.println("Employee Gross salary:"+employee.calculateGrossSalary());}
+            case 2 ->{
+                System.out.println("Manager Details:"+manager);
+                System.out.println("Manager :"+TaxUtil.calculateTax(manager));
+                System.out.println("manager Gross salary:"+trainer.calculateGrossSalary());
+            }
+            case 3->{
+                System.out.println("Trainer Details:"+trainer);
+                System.out.println("trainer :"+TaxUtil.calculateTax(trainer));
+                System.out.println("trainer Gross salary:"+trainer.calculateGrossSalary());
+            }
+            case 4->{
+                System.out.println("Sourcing Details:"+sourcing);
+                System.out.println("sourcing :"+TaxUtil.calculateTax(sourcing));
+                System.out.println("sourcing Gross salary:"+sourcing.calculateGrossSalary());
+            }
+            default -> System.out.println("please select from above only");
+        }
 
-        System.out.println("________________________________");
-        System.out.println("employee :"+TaxUtil.calculateTax(employee));
-        System.out.println("Employee Gross salary:"+employee.calculateGrossSalary());
-        System.out.println("________________________________");
-        System.out.println("Manager :"+TaxUtil.calculateTax(manager));
-        System.out.println("manager Gross salary:"+trainer.calculateGrossSalary());
-        System.out.println("________________________________");
-        System.out.println("trainer :"+TaxUtil.calculateTax(trainer));
-        System.out.println("trainer Gross salary:"+trainer.calculateGrossSalary());
-        System.out.println("________________________________");
-        System.out.println("sourcing :"+TaxUtil.calculateTax(sourcing));
-        System.out.println("sourcing Gross salary:"+sourcing.calculateGrossSalary());
     }
 }
