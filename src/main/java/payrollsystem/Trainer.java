a9a1c358a7833efea57c5358d6710da7be18eba6
Vinodambarapu267 <ambarapuvinod@gmail.com>
2025-08-06 12:18:35 +0530

    implemented the payroll System of an orgranization involves calculating the gross salary of Employee

---
+++ src/main/java/payrollsystem/Trainer.java
@@ -0,0 +1,24 @@
+package payrollsystem;
+
+public class Trainer {
+    private int trainerInt;
+    private String trainerName;
+    private  double basicSalary;
+    private  double HRAper;
+    private double DAper;
+    private int batchCount;
+    private double perkPerBatch;
+
+    public Trainer(int trainerInt, String trainerName, double basicSalary, double HRAper, double DAper, int batchCount, double perkPerBatch) {
+        this.trainerInt = trainerInt;
+        this.trainerName = trainerName;
+        this.basicSalary = basicSalary;
+        this.HRAper = HRAper;
+        this.DAper = DAper;
+        this.batchCount = batchCount;
+        this.perkPerBatch = perkPerBatch;
+    }
+    public  double calculateGrossSalary(){
+        return basicSalary+HRAper+DAper+(batchCount*perkPerBatch);
+    }
+}
