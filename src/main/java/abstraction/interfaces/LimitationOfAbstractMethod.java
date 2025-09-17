b51f09a4f559cb76878d6b547c0bf1095194094f
Vinodambarapu267 <ambarapuvinod@gmail.com>
2025-09-17 17:18:16 +0530

    limitation of Abstraction method

---
+++ src/main/java/abstraction/interfaces/LimitationOfAbstractMethod.java
@@ -0,0 +1,14 @@
+package abstraction.interfaces;
+
+import com.java.Main;
+
+import java.awt.event.MouseAdapter;
+
+public class LimitationOfAbstractMethod {
+    public static void main(String[] args) {
+        Vehicle v=null;
+        v= new Car();v.run();v.horn(); v.digitalMeter();
+        System.out.println("-----------------------------------------");
+        v=new Bike();v.run();v.horn();v.digitalMeter();
+    }
+}
